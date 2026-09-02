da21c3b7dea1b31bf012c3d7a58f1bc2695ed9a7
Flint Coffie <coffieflint91@gmail.com>
2026-09-02 15:50:28 +0000

    added sample posts and tab titles

--- phase_3/flask_blog/flask_blog.py
+++ phase_3/flask_blog/flask_blog.py
@@ -1,12 +1,33 @@
 # learning flask
+
+# importing flask and making use of the templates
 from flask import Flask, render_template
 
+# making an instance of flask
 app = Flask(__name__)
 
+# sample blog post
 
-@app.route("/")
+posts = [
+    {
+        "author": "Flint Coffie",
+        "title": "The advantages of being self taught",
+        "post_no": "Blog post 1",
+        "date_posted": "September 1, 2026",
+    },
+    {
+        "author": "Priscilla Coffie",
+        "title": "The disadvantages of being self taught",
+        "post_no": "Blog post 2",
+        "date_posted": "September 2, 2026",
+    },
+]
+
+
+# these are routes on the pages
+@app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
@@ -14,5 +35,6 @@ def about():
     return render_template("about.html")
 
 
+# if run directly changes will show and pages only need to be refreshed.
 if __name__ == "__main__":
     app.run(debug=True)
