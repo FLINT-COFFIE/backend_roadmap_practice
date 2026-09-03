84d88e53960d3882dd2f09366188c49a45ce26dd
Flint Coffie <coffieflint91@gmail.com>
2026-09-03 11:29:24 +0000

    added sqlalchemy db with data structure

--- phase_3/flask_api/api.py
+++ phase_3/flask_api/api.py
@@ -1,8 +1,24 @@
 # importing Flask
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 # making an instance of Flask
 app = Flask(__name__)
+
+# location and name of db
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+
+# user data
+class UserModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+
+    # user info
+    def __repr__(self):
+        return f"User(name = {self.name}, email = {self.email}"
 
 
 # making routes
