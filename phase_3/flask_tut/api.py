8ce7405b62205b3cb979d9f0debf3d66e206411a
Flint Coffie <coffieflint91@gmail.com>
2026-09-04 12:13:15 +0000

    create and read works

--- phase_3/flask_tut/api.py
+++ phase_3/flask_tut/api.py
@@ -36,13 +36,41 @@ books_list = [
 ]
 
 
+# default route
+@app.route("/")
+def home():
+    return "Welcome"
+
+
 # create and read route for books
 @app.route("/books", methods=["GET", "POST"])
 # view function
 def books():
+    # reading the books
     if request.method == "GET":
         if len(books_list) > 0:
             # returning the books as a response
             return jsonify(books_list)
         else:
             "Nothing Found", 404  # error code
+
+    # creating a new book
+    if request.method == "POST":
+        new_author = request.form["author"]
+        new_lang = request.form["language"]
+        new_title = request.form["title"]
+        new_id = books_list[-1]["id"] + 1
+
+        new_book = {
+            "id": new_id,
+            "author": new_author,
+            "language": new_lang,
+            "title": new_title,
+        }
+
+        books_list.append(new_book)
+        return jsonify(books_list), 201  # success code
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
