08916a3699ce47b2b7f216708b2d5b41814bcf66
Flint Coffie <coffieflint91@gmail.com>
2026-09-06 13:31:07 +0000

    added a query parameter and made it optional

--- phase_4/fast_api_tut_1/api.py
+++ phase_4/fast_api_tut_1/api.py
@@ -1,5 +1,6 @@
 # importing FastAPI
 from fastapi import FastAPI, Path
+from typing import Optional
 
 # creating an instance of FastAPI
 app = FastAPI()
@@ -15,8 +16,20 @@ def home():
 
 
 # making the path to return students
+# getting by student id (path parameter)
 @app.get("/get-student/{student_id}")
-def get_student(
+def get_student_id(
+    # Put ... an elipsis to mark it as required
     student_id: int = Path(..., description="The ID of the student", gt=0, lt=3),
 ):
     return students[student_id]
+
+
+# Query parameter
+# getting by student name
+@app.get("/get-by-name")
+def get_student_name(name: Optional[str] = None):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"404": "Not Found"}
