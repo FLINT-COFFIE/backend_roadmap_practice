7e7bdf26d06dbf3ab16f99953695ea3cc4cce292
Flint Coffie <coffieflint91@gmail.com>
2026-09-06 16:45:59 +0000

    completed the crud with fast_api

--- phase_4/fast_api_tut_1/api.py
+++ phase_4/fast_api_tut_1/api.py
@@ -75,3 +75,15 @@ def update_student(student_id: int, student: UpdateStudent):
         students[student_id].year = student.year
 
     return students[student_id]
+
+
+# adding the delete method
+@app.delete("/delete-student/{student_id}")
+def delete_student(
+    student_id: int,
+):
+    if students[student_id] not in students:
+        return {"Error": "Student does not exist"}
+
+    del students[student_id]
+    return {"Message": "Student deleted successfully"}
