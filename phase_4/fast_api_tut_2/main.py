89bb2a6b62ddab14c7f281ca46e3db334c900071
Flint Coffie <coffieflint91@gmail.com>
2026-09-12 14:34:47 +0000

    learning further about fastapi

--- phase_4/fast_api_tut_2/main.py
+++ phase_4/fast_api_tut_2/main.py
@@ -1,8 +1,32 @@
+# Imports
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
+# Instance of fastapi
 app = FastAPI()
 
 
+# making a post base model
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+# path operations
 @app.get("/")
-async def root():
+def root():
     return {"message": "Hello World"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
+
+
+@app.post("/posts")
+def create_posts(post: Post):
+    print(post.title)
+    return {"data": "new post"}
