3aeaaaa64a30f6c1384c01d068b6b2857becbf6d
Flint Coffie <coffieflint91@gmail.com>
2026-09-12 20:33:41 +0000

    added delete

--- phase_4/fast_api_tut_2/main.py
+++ phase_4/fast_api_tut_2/main.py
@@ -24,6 +24,21 @@ def find_post(id):
             return p
 
 
+# finding post index
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
+
+
+# error post
+def raise_404(id):
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"post with id {id} was not found",
+    )
+
+
 # saving posts in memory
 my_posts = [
     {
@@ -64,16 +79,21 @@ def get_posts():
 def get_one_post(id: int):
     post = find_post(id)
     if not post:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"post with id {id} was not found",
-        )
+        raise_404(id)
     return {"data": post}
 
 
-@app.post("/posts")
-def create_posts(post: Post):
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
     post_dict = post.model_dump()
     post_dict["id"] = randrange(0, 1000000000000)
     my_posts.append(post_dict)
     return {"data": post_dict}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index is None:
+        raise_404(id)
+    my_posts.pop(index)
